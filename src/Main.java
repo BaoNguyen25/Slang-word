ed1279e65e6a810042b2dc871ffa69760a105af0
baonguyen25 <nguyenminhgiabao2501@gmail.com>
2022-11-18 15:02:31 +0700

    create basic menu

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         SlangApplication app = new SlangApplication();
-        app.run();
+        app.init();
     }
 }
---
+++ src/SlangApplication.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class SlangApplication extends JFrame {
+
+    public SlangApplication() {
+    }
+
+    public void init() {
+
+        this.setTitle("Slang Words Application");
+        this.setSize(400, 600);
+        this.setLayout(new BorderLayout());
+
+        // create header
+        JLabel header = new JLabel("MENU", JLabel.CENTER);
+        Font font  = new Font(Font.SANS_SERIF,  Font.BOLD, 20);
+        header.setFont(font);
+        header.setForeground(Color.RED);
+        JPanel jPanel_header = new JPanel();
+        jPanel_header.setLayout(new BorderLayout());
+        jPanel_header.add(header, BorderLayout.CENTER);
+
+        // create list of features
+        JPanel jPanel_features = addButton();
+
+        this.add(jPanel_header, BorderLayout.NORTH);
+        this.add(jPanel_features, BorderLayout.CENTER);
+
+        this.setDefaultLookAndFeelDecorated(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public JPanel addButton() {
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS));
+
+        buttonPanel.add(createButton("View Slang Words List"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Find Slang By Word"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Find Slang By Definition"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("View Search History"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Add Slang Word"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Edit Slang Word"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Delete Slang Word"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Reset Slang Words List"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Random Slang Word"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        buttonPanel.add(createButton("Game"));
+        buttonPanel.add(Box.createVerticalGlue());
+
+        return buttonPanel;
+    }
+
+    public JButton createButton(String text) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        button.setFocusable(false);
+
+        return button;
+    }
+}
